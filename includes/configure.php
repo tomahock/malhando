83376547ba94277dfc617d38ea316dddc6dd097b
tomahock <eu@tomahock.com>
2013-11-17 21:39:04 +0000

    clean up and mysql settings

--- includes/configure.php
+++ includes/configure.php
@@ -8,8 +8,8 @@
  */
 
 // set developer or prod
-//$mode = 'dev';
-$mode = 'prod';
+$mode = 'dev';
+//$mode = 'prod';
 
 if( $mode === 'dev' ) {
 	define( 'DEVELOPER', true );
@@ -17,30 +17,16 @@ if( $mode === 'dev' ) {
 	define( 'DEVELOPER', false );
 }
 
-// database configs
-//$database =  array(
-//	'host' => 'labmm.clients.ua.pt',
-//	'user' => '2012_lm4_g12',
-//	'pass' => 'mxvk',
-//	'table' => '2012_lm4_g12_b01'
-//);
-
 
 $database =  array(
 	'host' => 'localhost',
-	'user' => 'malha',
-	'pass' => 'm@lh@',
-	'table' => 'malha'
+	'user' => 'CHANGE-HERE',
+	'pass' => 'CHANGE-HERE',
+	'table' => 'CHANGE-HERE'
 );
+
 
 define( 'DATABASE', serialize( $database ) );
 
 // salt for password hashing
 DEFINE( 'SALT', 'w[lWeLXs.PJzvCP.ia9*;%%P.o`wt< 2G+_ dan_Lz{2s2]2eokc]>0lN?mTP-%;' );
-
-//
-DEFINE( 'HOME_PATH', '/home/proj/public_html' );
-//DEFINE( 'HOME_PATH', '/2012_lm4_g12/campFire/' );
-
-//default lang
-$defaultLang = 'pt';
